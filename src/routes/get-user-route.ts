b4140c45aa53467dd5fba769fe7b097069b7427e
Diego Sampaio <diiegosmp@gmail.com>
2024-09-11 15:20:48 -0300

    Add feature route create user

--- src/routes/get-user-route.ts
+++ src/routes/get-user-route.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express'
+import { CreateUserController } from '../controllers/CreateUserController'
 
 const route = Router()
+
+route.post('/create', new CreateUserController().handle)
 
 export default route
