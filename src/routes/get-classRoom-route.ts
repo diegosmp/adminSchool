5d82741ee30216baa10694bac0057ff67104adfd
diegosmp <diiegosmp@gmail.com>
2024-09-13 10:49:26 -0300

    feature: route edit class room

--- src/routes/get-classRoom-route.ts
+++ src/routes/get-classRoom-route.ts
@@ -3,11 +3,13 @@ import { isAuthenticated } from '../middlewares/isAuthenticated'
 import { CreateClassRoomController } from '../controllers/classroom/CreateClassRoomController'
 import { DetailsClassRoomController } from '../controllers/classroom/DetailsClassRoomController'
 import { AllClassRoomController } from '../controllers/classroom/AllClassRoomController'
+import { EditClassRoomController } from '../controllers/classroom/EditClassRoomController'
 
 const route = Router()
 
 route.post('/create', isAuthenticated, new CreateClassRoomController().handle)
 route.get('/details', isAuthenticated, new DetailsClassRoomController().handle)
 route.get('/all', isAuthenticated, new AllClassRoomController().handle)
+route.put('/details/edit', isAuthenticated, new EditClassRoomController().handle)
 
 export default route
