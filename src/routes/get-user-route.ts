7e85e19085cde727391c68dc610f79ade29ca791
diegosmp <diiegosmp@gmail.com>
2024-09-12 22:49:11 -0300

    feature (route delete)

--- src/routes/get-user-route.ts
+++ src/routes/get-user-route.ts
@@ -4,6 +4,7 @@ import { AuthUserController } from '../controllers/AuthUserController'
 import { isAuthenticated } from '../middlewares/isAuthenticated'
 import { ProfileUserController } from '../controllers/ProfileUserController'
 import { EditUserController } from '../controllers/EditUserController'
+import { DeleteUserController } from '../controllers/DeleteUserController'
 
 const route = Router()
 
@@ -11,5 +12,6 @@ route.post('/create', new CreateUserController().handle)
 route.post('/signin', new AuthUserController().handle)
 route.get('/profile', isAuthenticated, new ProfileUserController().handle)
 route.put('/profile', isAuthenticated, new EditUserController().handle)
+route.delete('/profile', isAuthenticated, new DeleteUserController().handle)
 
 export default route
