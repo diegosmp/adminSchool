e4926add999d3124c32e2609bd716ed65afad03f
diegosmp <diiegosmp@gmail.com>
2024-09-13 09:58:07 -0300

    feature (class rom): profile class room controller

---
+++ src/controllers/classroom/ProfileClassRoomController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express'
+import { ProfileClassRoomService } from '../../services/classroom/ProfileClassRoomService'
+
+class ProfileClassRoomController {
+  async handle(req: Request, res: Response) {
+    const classId = req.query.classId as string
+    const profileClassRoomService = new ProfileClassRoomService()
+    const classRoom = profileClassRoomService.execute(classId)
+
+    return res.json(classRoom)
+  }
+}
+
+export { ProfileClassRoomController }
