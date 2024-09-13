e195c0a0352ffcd2652f7b925a22423a6f9781f4
diegosmp <diiegosmp@gmail.com>
2024-09-13 13:26:52 -0300

    feature: add route detail monitor

--- src/routes/get-monior-route.ts
+++ src/routes/get-monior-route.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { isAuthenticated } from '../middlewares/isAuthenticated'
 import { CreateMonitorController } from '../controllers/monitor/CreateMonitorController'
+import { DetailMonitorController } from '../controllers/monitor/DetailMonitorController'
 
 const route = Router()
 
 route.post('/create', isAuthenticated, new CreateMonitorController().handle)
+route.get('/detail', isAuthenticated, new DetailMonitorController().handle)
 
 export default route
