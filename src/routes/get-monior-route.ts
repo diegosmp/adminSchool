53d62fe20b662fb7a76da7901256c9bba2dd2a27
diegosmp <diiegosmp@gmail.com>
2024-09-13 13:35:57 -0300

    feature: route edit and delete monitor

--- src/routes/get-monior-route.ts
+++ src/routes/get-monior-route.ts
@@ -2,10 +2,14 @@ import { Router } from 'express'
 import { isAuthenticated } from '../middlewares/isAuthenticated'
 import { CreateMonitorController } from '../controllers/monitor/CreateMonitorController'
 import { DetailMonitorController } from '../controllers/monitor/DetailMonitorController'
+import { EditMonitorController } from '../controllers/monitor/EditMonitorController'
+import { DeleteMonitorController } from '../controllers/monitor/DeleteMonitorController'
 
 const route = Router()
 
 route.post('/create', isAuthenticated, new CreateMonitorController().handle)
 route.get('/detail', isAuthenticated, new DetailMonitorController().handle)
+route.put('/edit', isAuthenticated, new EditMonitorController().handle)
+route.delete('/delete', isAuthenticated, new DeleteMonitorController().handle)
 
 export default route
