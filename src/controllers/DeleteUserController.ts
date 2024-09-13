403bafc0465993a55cab96e8cb6c35d415d0d9da
diegosmp <diiegosmp@gmail.com>
2024-09-12 22:53:47 -0300

    refactore (delete): refactor delete user controller

--- src/controllers/DeleteUserController.ts
+++ src/controllers/DeleteUserController.ts
@@ -3,7 +3,9 @@ import { DeleteUserService } from '../services/user/DeleteUserService'
 
 class DeleteUserController {
   async handle(req: Request, res: Response) {
-    const user_id = req.user_id as string
+    const user_id = req.query.user_id as string
+
+    console.log(user_id)
 
     const deleteUserService = new DeleteUserService()
     const user = await deleteUserService.execute(user_id)
