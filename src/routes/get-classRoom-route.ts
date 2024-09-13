4745bcef343734dbacaf73ef0faf78b225a45975
diegosmp <diiegosmp@gmail.com>
2024-09-13 10:16:50 -0300

    feature: add route all show class room

--- src/routes/get-classRoom-route.ts
+++ src/routes/get-classRoom-route.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 import { isAuthenticated } from '../middlewares/isAuthenticated'
 import { CreateClassRoomController } from '../controllers/classroom/CreateClassRoomController'
 import { ProfileClassRoomController } from '../controllers/classroom/ProfileClassRoomController'
+import { AllClassRoomController } from '../controllers/classroom/AllClassRoomController'
 
 const route = Router()
 
 route.post('/create', isAuthenticated, new CreateClassRoomController().handle)
 route.get('/details', isAuthenticated, new ProfileClassRoomController().handle)
+route.get('/all', isAuthenticated, new AllClassRoomController().handle)
 
 export default route
