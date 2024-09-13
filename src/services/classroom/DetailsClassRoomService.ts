4c9dd6d9ef903ae906f29d2d2d6a1949aa1d9a9d
diegosmp <diiegosmp@gmail.com>
2024-09-13 15:57:13 -0300

    refactor: details include for show monitor, students and teacher

--- src/services/classroom/DetailsClassRoomService.ts
+++ src/services/classroom/DetailsClassRoomService.ts
@@ -2,7 +2,10 @@ import { prismaCliente } from '../../prisma/prisma'
 
 class DetailsClassRoomService {
   async execute(classId: string) {
-    const classRoom = await prismaCliente.classRoom.findFirst({ where: { id: classId } })
+    const classRoom = await prismaCliente.classRoom.findFirst({
+      where: { id: classId },
+      include: { students: true, monitor: true, teacher: true },
+    })
     return classRoom
   }
 }
