d0b1e75275f5b187d315d793c3a529cba02bc647
diegosmp <diiegosmp@gmail.com>
2024-09-13 10:00:30 -0300

    feature(route): profile class room

--- src/routes/get-classRoom-route.ts
+++ src/routes/get-classRoom-route.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { isAuthenticated } from '../middlewares/isAuthenticated'
 import { CreateClassRoomController } from '../controllers/classroom/CreateClassRoomController'
+import { ProfileClassRoomController } from '../controllers/classroom/ProfileClassRoomController'
 
 const route = Router()
 
 route.post('/create', isAuthenticated, new CreateClassRoomController().handle)
+route.get('/profile', isAuthenticated, new ProfileClassRoomController().handle)
 
 export default route
