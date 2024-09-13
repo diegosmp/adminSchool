3395333c05d333c594f846a75e6a470931897f07
diegosmp <diiegosmp@gmail.com>
2024-09-12 21:40:39 -0300

    feature (route): Add route profile

--- src/routes/get-user-route.ts
+++ src/routes/get-user-route.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express'
 import { CreateUserController } from '../controllers/CreateUserController'
+import { AuthUserController } from '../controllers/AuthUserController'
+import { isAuthenticated } from '../middlewares/isAuthenticated'
+import { ProfileUserController } from '../controllers/ProfileUserController'
 
 const route = Router()
 
 route.post('/create', new CreateUserController().handle)
+route.post('/signin', new AuthUserController().handle)
+route.get('/profile', isAuthenticated, new ProfileUserController().handle)
 
 export default route
