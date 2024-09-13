47b36667e7f3bb309a496ee09323259a4d6ceac8
diegosmp <diiegosmp@gmail.com>
2024-09-13 09:07:18 -0300

    feature (student): create feature controller

---
+++ src/controllers/student/CreateStudentController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express'
+import { CreateStudentService } from '../../services/student/CreateStudentService'
+
+class CreateStudentController {
+  async handle(res: Response, req: Request) {
+    const { fullname, cpf, classId } = req.body
+    const createStudentService = new CreateStudentService()
+    const student = await createStudentService.execute({ fullname, cpf, classId })
+
+    return res.json(student)
+  }
+}
+
+export { CreateStudentController }
