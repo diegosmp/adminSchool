2aa3769f75a8c95effe31a785b2089102b1a9452
diegosmp <diiegosmp@gmail.com>
2024-09-12 21:35:19 -0300

    feature (profile): add feature profile controller

---
+++ src/controllers/ProfileUserController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express'
+import { ProfileUserService } from '../services/user/ProfileUserService'
+
+class ProfileUserController {
+  async handle(req: Request, res: Response) {
+    const user_id = req.user_id
+
+    const profileUserService = new ProfileUserService()
+    const user = await profileUserService.execute(user_id)
+
+    return res.json(user)
+  }
+}
+
+export { ProfileUserController }
