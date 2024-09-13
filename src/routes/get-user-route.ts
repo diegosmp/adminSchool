e4ea7b87f4147605a78837d36c8748a48c5f8f57
diegosmp <diiegosmp@gmail.com>
2024-09-12 22:18:31 -0300

    feature(route): add route edit user

--- src/routes/get-user-route.ts
+++ src/routes/get-user-route.ts
@@ -3,11 +3,13 @@ import { CreateUserController } from '../controllers/CreateUserController'
 import { AuthUserController } from '../controllers/AuthUserController'
 import { isAuthenticated } from '../middlewares/isAuthenticated'
 import { ProfileUserController } from '../controllers/ProfileUserController'
+import { EditUserController } from '../controllers/EditUserController'
 
 const route = Router()
 
 route.post('/create', new CreateUserController().handle)
 route.post('/signin', new AuthUserController().handle)
 route.get('/profile', isAuthenticated, new ProfileUserController().handle)
+route.put('/profile', isAuthenticated, new EditUserController().handle)
 
 export default route
