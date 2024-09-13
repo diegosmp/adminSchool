d3afea086825aa7b47a8040715b63cfff99282e6
diegosmp <diiegosmp@gmail.com>
2024-09-13 09:28:12 -0300

    fix (student): controller create

--- src/controllers/student/CreateStudentController.ts
+++ src/controllers/student/CreateStudentController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 import { CreateStudentService } from '../../services/student/CreateStudentService'
 
 class CreateStudentController {
-  async handle(res: Response, req: Request) {
+  async handle(req: Request, res: Response) {
     const { fullname, cpf, classId } = req.body
     const createStudentService = new CreateStudentService()
     const student = await createStudentService.execute({ fullname, cpf, classId })
