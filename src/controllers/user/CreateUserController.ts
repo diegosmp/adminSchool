5d7d9f9dc26bd83376133ed32a6ced80dae1ebf6
diegosmp <diiegosmp@gmail.com>
2024-09-18 22:59:15 -0300

    update controller create and edit users

--- src/controllers/user/CreateUserController.ts
+++ src/controllers/user/CreateUserController.ts
@@ -3,10 +3,10 @@ import { CreateUserService } from '../../services/user/CreateUserService'
 
 class CreateUserController {
   async handle(req: Request, res: Response) {
-    const { email, password, fullname } = req.body
+    const { email, password, firstname, secondname } = req.body
 
     const createUserService = new CreateUserService()
-    const user = await createUserService.execute({ email, password, fullname })
+    const user = await createUserService.execute({ email, password, firstname, secondname })
 
     return res.status(201).json(user)
   }
--- src/controllers/user/EditUserController.ts
+++ src/controllers/user/EditUserController.ts
@@ -3,11 +3,11 @@ import { EditUserService } from '../../services/user/EditUserService'
 
 class EditUserController {
   async handle(req: Request, res: Response) {
-    const { email, fullname } = req.body
+    const { email, firstname, secondname } = req.body
     const user_id = req.query.user_id as string
 
     const editUserService = new EditUserService()
-    const user = await editUserService.execute({ email, fullname, user_id })
+    const user = await editUserService.execute({ email, firstname, secondname, user_id })
 
     return res.json(user)
   }
